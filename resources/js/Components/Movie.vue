3f96cfebdc03e4735d6b5c964938f4703dab0808
chariss <h.vairlis@gmail.com>
2021-02-18 23:12:17 +0200

    change movie component stylish

--- resources/js/Components/Movie.vue
+++ resources/js/Components/Movie.vue
@@ -1,17 +1,33 @@
 <template>
     <div>
-        <div class="flex bg-white rounded-lg mx-4 md:mx-auto my-6 max-w-md md:max-w-4xl "><!--horizantil margin is just for display-->
-            <div class="flex items-start px-4 py-6">
+        <div class="bg-white rounded-lg shadow mx-4 md:mx-auto my-6 max-w-md md:max-w-2xl">
+            <div class="flex justify-between px-6 py-6">
                 <div>
                     <jet-application-logo class="block h-12 w-auto w-12 h-12 mr-4 rounded-xl" />
                 </div>
-                <div class="">
+                 <div class="items-start">
+                    <div>
+                        <h2 class="text-lg font-semibold text-gray-900">{{ movie.title }}</h2>
+                    </div>
+                    <div>
+                        <p v-if="movie.subtitle" class="text-gray-700"> {{ movie.subtitle }} </p>
+                    </div>
+                </div>
+                <div>
                     <div class="flex items-center justify-between">
-                        <h2 class="text-lg font-semibold text-gray-900 -mt-1">{{ movie.title }}</h2>
                         <small class="text-sm text-gray-700">Added at: {{ movie.created_at}}</small>
                     </div>
-                    <p v-if="movie.subtitle" class="text-gray-700"> {{ movie.subtitle }} </p>
-                    <p class="mt-3 text-gray-700 text-sm"> {{ movie.description }} </p>
+                </div>
+            </div>
+
+            <div class="px-6">
+                <div>
+                    <p class="mt-3 text-gray-700 text-sm text-center"> {{ movie.description }} </p>
+                </div>
+            </div>
+
+            <div class="flex justify-between px-6 py-6">
+                <div class="flex flex-wrap content-end">
                     <div class="mt-4 flex items-center">
                         <!-- likes -->
                         <like-button :likes="movie.likes" />
@@ -21,9 +37,10 @@
 
                         <!-- loves -->
                         <love-button :loves="movie.loves" />
-
-                        <small class="text-sm text-gray-700 pl-6">Posted by: {{ movie.user.name }} </small>
                     </div>
+                </div>
+                <div class="flex flex-wrap content-end">
+                    <small class="text-sm text-gray-700 pl-6">Posted by: {{ movie.user.name }} </small>
                 </div>
             </div>
         </div>
